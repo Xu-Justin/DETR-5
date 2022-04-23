427581165dbe04c7988c9fd03de06180cc5359e1
William Justin <williamjustinxu@gmail.com>
2022-04-23 14:22:20 +0700

    add files

---
+++ my_dataset/link.py
@@ -0,0 +1,12 @@
+import os
+def link(source, target):
+    os.makedirs(target, exist_ok=True)
+    print(f"Create symbolic link from {source} to {target}")
+    os.system(f"ln -s -r {source}* {target}")
+    
+root = 'nodeflux-vehicle'
+
+link(os.path.join(root, 'mixed/train/images/'), 'train/images/')
+link(os.path.join(root, 'mixed/train/annotations.json'), 'train/annotations.json')
+link(os.path.join(root, 'mixed/val/images/'), 'val/images/')
+link(os.path.join(root, 'mixed/val/annotations.json'), 'val/annotations.json')
